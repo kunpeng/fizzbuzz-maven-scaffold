d5756ddb9427915c6599f3c58140623b21a49d3b
唐昆鹏 <tangkp001@163.com>
2019-12-28 11:38:00 +0800

    pass

--- src/test/java/FizzBuzz/FizzBuzz.java
+++ src/test/java/FizzBuzz/FizzBuzz.java
@@ -2,9 +2,18 @@ package FizzBuzz;
 
 public class FizzBuzz {
     public static String calc(int number){
-        if(number %3 == 0){
+        if(number % 3 == 0 && number % 5 == 0){
+            return "FizzBuzz";
+        }
+
+        if(number % 3 == 0){
             return "Fizz";
         }
+
+        if(number % 5 == 0){
+            return "Buzz";
+        }
+        
         return String.valueOf(number);
     }
 }
